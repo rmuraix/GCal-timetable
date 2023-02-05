e0df4f7f18af4186e393a21bb7e2913977f6ee07
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-02-06 00:11:08 +0900

    Holyday

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-const getEnv = (name:string) => {
+import { Cal } from "./calender";
+
+const getEnv = (name: string) => {
     const properties = PropertiesService.getScriptProperties();
     return properties.getProperty(name) || "";
+}
+
+const isHolyday = (holydayCal: Cal, date: Date): boolean => {
+    const event = holydayCal.getAllDayEvent(date);
+    if (event) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 const main = () => {
@@ -9,4 +20,6 @@ const main = () => {
         myCalId: getEnv('MY_CAL'),
         startDate: new Date()
     };
+    const holydayCal = new Cal(config.holydayCalId);
+    const myCal = new Cal(config.myCalId);
 }
