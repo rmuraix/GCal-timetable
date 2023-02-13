6e0a6711db6f682a9cfc6746be1c591c0dfdce82
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-02-14 04:47:57 +0900

    Create events

--- src/main.ts
+++ src/main.ts
@@ -15,19 +15,42 @@ const isHolyday = (holydayCal: Cal, date: Date): boolean => {
     }
 }
 
-const main = () => {
+export const main = () => {
     const config = {
         // Calendar settings
         holydayCalId: 'ja.japanese#holiday@group.v.calendar.google.com',
         myCalId: getEnv('MY_CAL'),
         // Setting about timetable
         startDate: new Date(),
-        startTime: ["9:20", "11:15", "13:50", "15:45"],
+        startTime: [[9, 20], [11, 15], [13, 50], [15, 45]],
         // You need to create a secret.ts and create an array.
         subject: subject,
         minutes: 105,
-        count: 13
+        count: 2
     };
     const holydayCal = new Cal(config.holydayCalId);
     const myCal = new Cal(config.myCalId);
+
+    let targetDate = config.startDate
+
+    for (let i = 0; i < config.count; i++) {
+        // a week
+        let num = 0;
+        do {
+            // a day
+            for (let j = 0; j < 4; j++) {
+                if (config.subject[num][j][0] != "") {
+                    myCal.createEvent(
+                        config.subject[num][j][0],
+                        new Date(targetDate.setHours(config.startTime[j][0], config.startTime[j][1])),
+                        new Date(targetDate.setMinutes(targetDate.getMinutes() + config.minutes)),
+                        config.subject[num][j][1]
+                    );
+                }
+            }
+            targetDate = new Date(targetDate.setDate(targetDate.getDate() + 1));
+            num++;
+        } while (targetDate.getDay() < 6);
+        targetDate = new Date(targetDate.setDate(targetDate.getDate() + 2));
+    }
 }
