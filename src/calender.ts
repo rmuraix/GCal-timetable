c84a1f40d47aba761561a32913a4fd5c4822ec58
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-04-04 14:02:03 +0900

    Format code

--- src/calender.ts
+++ src/calender.ts
@@ -1,29 +1,32 @@
-import { deleteEvents } from "./main";
-
 export class Cal {
-    private readonly calendar: GoogleAppsScript.Calendar.Calendar;
-    constructor(id: string) {
-        this.calendar = CalendarApp.getCalendarById(id);
+  private readonly calendar: GoogleAppsScript.Calendar.Calendar;
+  constructor(id: string) {
+    this.calendar = CalendarApp.getCalendarById(id);
+  }
+  isAlldayEvent(event: GoogleAppsScript.Calendar.CalendarEvent): boolean {
+    return event.isAllDayEvent();
+  }
+  getAllDayEvent(date: Date) {
+    const events = this.calendar.getEventsForDay(date);
+    if (events[0]) {
+      return events[0];
+    } else {
+      return null;
     }
-    isAlldayEvent(event: GoogleAppsScript.Calendar.CalendarEvent): boolean {
-        return event.isAllDayEvent();
-    }
-    getAllDayEvent(date: Date) {
-        const events = this.calendar.getEventsForDay(date);
-        if(events[0]){
-            return events[0];
-        }else{
-            return null;
-        }
-    }
-    getEvents(startTime:Date, endTime:Date) {
-        return this.calendar.getEvents(startTime,endTime);
-    }
-    createEvent(title: string, startTime: Date, endTime: Date, description: string) {
-        const options = { description: description };
-        this.calendar.createEvent(title, startTime, endTime, options);
-    }
-    deleteEvent(event:GoogleAppsScript.Calendar.CalendarEvent) {
-        event.deleteEvent();
-    }
+  }
+  getEvents(startTime: Date, endTime: Date) {
+    return this.calendar.getEvents(startTime, endTime);
+  }
+  createEvent(
+    title: string,
+    startTime: Date,
+    endTime: Date,
+    description: string
+  ) {
+    const options = { description: description };
+    this.calendar.createEvent(title, startTime, endTime, options);
+  }
+  deleteEvent(event: GoogleAppsScript.Calendar.CalendarEvent) {
+    event.deleteEvent();
+  }
 }
